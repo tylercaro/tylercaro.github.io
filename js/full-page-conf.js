95ee25ebcbc92facc7239848fc279aaa3894f040
tylercaro <tylercaro@gmail.com>
2016-09-22 21:18:11 -0500

    Adjust responsive typography.

--- js/full-page-conf.js
+++ js/full-page-conf.js
@@ -2,10 +2,8 @@ $(document).ready(function() {
 	$('#fullpage').fullpage({
 		sectionsColor: ['#f2f2f2', '#f2f2f2', '#FCF9E2'],
 		scrollingSpeed: 600,
-		continuousVertical: true, 
-		loopBottom: false,
+		loopBottom: true,
 		scrollBar: false,
-		touchSensitivity: 15,
 		anchors: ['introPage', 'skillsPage', 'personalPage'],
 		menu: "#menu"
 	});
